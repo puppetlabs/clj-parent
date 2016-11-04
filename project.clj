2d28982ac5ca684b3546e6f90b096b2df20af9d6
Chris Price <chris@puppetlabs.com>
2016-11-04 14:24:59 -0700

    (MAINT) Update ring-json

--- project.clj
+++ project.clj
@@ -50,7 +50,7 @@
                          [ring/ring-servlet "1.5.0"]
                          [ring/ring-core "1.4.0"]
                          [ring/ring-codec "1.0.0"]
-                         [ring/ring-json "0.3.1"]
+                         [ring/ring-json "0.4.0"]
                          [ring-basic-authentication "1.0.5"]
                          [ring-mock "0.1.5"]
                          [beckon "0.1.1"]
