0525fe75b7c42fa79d346fa96f2ae35df22f3fb4
Justin Stoller <justin.stoller@gmail.com>
2018-05-23 08:22:23 -0700

    (SERVER-2205) Upgrade ssl-utils to 1.0.0

--- project.clj
+++ project.clj
@@ -95,7 +95,7 @@
                          [puppetlabs/http-client "0.9.0"]
                          [puppetlabs/jdbc-util "1.1.1"]
                          [puppetlabs/typesafe-config "0.1.5"]
-                         [puppetlabs/ssl-utils "0.9.1"]
+                         [puppetlabs/ssl-utils "1.0.0"]
                          [puppetlabs/clj-ldap "0.1.6"]
                          [puppetlabs/kitchensink ~ks-version]
                          [puppetlabs/kitchensink ~ks-version :classifier "test"]
