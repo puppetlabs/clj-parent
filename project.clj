20e77600f329815acdb8554605a561d47ef406cc
jonathannewman <jonathan.newman@puppetlabs.com>
2018-10-11 10:08:58 -0700

    (maint) update clj-rbac-client to 0.9.2
    Version 0.9.2 has the route limit removed from the clj-http-client which
    will increase request throughput for rbac and activity service requests.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 (def tk-jetty-version "2.3.0")
 (def tk-metrics-version "1.2.0")
 (def logback-version "1.2.3")
-(def rbac-client-version "0.9.1")
+(def rbac-client-version "0.9.2")
 (def dropwizard-metrics-version "3.2.2")
 
 (defproject puppetlabs/clj-parent "2.3.5-SNAPSHOT"
