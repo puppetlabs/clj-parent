48ae5db1181946f04c21d02fb16dd59d36c6dd36
jonathannewman <jonathan.newman@puppetlabs.com>
2018-10-11 09:08:22 -0700

    (maint) update clj-rbac-client to 0.8.4
    Version 0.8.4 changes the per route limit to 20, which will increase
    the overall throughput of the client when in heavy use.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 (def tk-jetty-version "2.1.2")
 (def tk-metrics-version "1.2.0")
 (def logback-version "1.1.9")
-(def rbac-client-version "0.8.3")
+(def rbac-client-version "0.8.4")
 (def dropwizard-metrics-version "3.2.2")
 
 (defproject puppetlabs/clj-parent "1.7.16-SNAPSHOT"
