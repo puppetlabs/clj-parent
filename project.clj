85919143cb8dfff5be8ce359e93b4d29c0e1e89f
Katie Lawhorn <katie.lawhorn@puppet.com>
2019-02-25 11:11:08 -0800

    (maint): update jdbc-util version for jsonb methods

--- project.clj
+++ project.clj
@@ -94,7 +94,7 @@
                          [prismatic/schema "1.1.9"]
 
                          [puppetlabs/http-client "1.0.0"]
-                         [puppetlabs/jdbc-util "1.2.3"]
+                         [puppetlabs/jdbc-util "1.2.4"]
                          [puppetlabs/typesafe-config "0.1.5"]
                          [puppetlabs/ssl-utils "1.0.2"]
                          [puppetlabs/clj-ldap "0.2.0"]
