2416a37ed19772cf300a7b63e2ef4184e907e9af
jonathannewman <jonathan.newman@puppetlabs.com>
2020-08-10 16:43:01 -0700

    (PE-29720) bump clj-rbac-client to 1.0.0
    This updates clj-rbac-client to 1.0.0 to add support for the v2
    activity submission endpoint.  If the service does not support
    the v2 endpoint, it will gracefully fall back to the v1 endpoint.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 (def tk-jetty-version "4.1.0")
 (def tk-metrics-version "1.3.1")
 (def logback-version "1.2.3")
-(def rbac-client-version "0.9.4")
+(def rbac-client-version "1.0.0")
 (def dropwizard-metrics-version "3.2.2")
 
 (defproject puppetlabs/clj-parent "4.6.1-SNAPSHOT"
