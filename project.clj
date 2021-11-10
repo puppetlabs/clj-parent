0b0d3149b3918e400cc8465ba9eb8f8e0cf8bc22
Maggie Dreyer <maggie@puppet.com>
2021-11-10 08:21:47 -0800

    (maint) Switch back to public release repos

--- project.clj
+++ project.clj
@@ -140,8 +140,8 @@
 
   :dependencies [[org.clojure/clojure]]
 
-  :deploy-repositories [["releases" {:url "https://artifactory.delivery.puppetlabs.net/artifactory/clojure-releases__local/"
-                                     :username :env/nexus_jenkins_username
-                                     :password :env/nexus_jenkins_password
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
                                      :sign-releases false}]
                         ["snapshots" "https://artifactory.delivery.puppetlabs.net/artifactory/list/clojure-snapshots__local/"]])
