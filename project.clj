ffd72396c13bca131a830da51dcbf79ad8b0eb96
Jenkins CI <ci@puppet.com>
2022-10-01 22:04:18 +0000

    Version 5.2.9

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 (def rbac-client-version "1.1.3")
 (def dropwizard-metrics-version "3.2.2")
 
-(defproject puppetlabs/clj-parent "5.2.9-SNAPSHOT"
+(defproject puppetlabs/clj-parent "5.2.9"
   ;; Abort when version ranges or version conflicts are detected in
   ;; dependencies. Also supports :warn to simply emit warnings.
   ;; requires lein 2.2.0+.
