d5ee99063b552c5309ceaf0fb8cda2d7ac449d96
jonathannewman <jonathan.newman@puppetlabs.com>
2024-05-22 16:31:03 -0700

    (maint) update clj-shell-utils to 2.0.1
    If a cwd is specified, pass it through to the executor. This is necessary so
    that puppet's generate function doesn't modify the cwd for the entire JRuby
    process[1].
    
    [1] https://github.com/puppetlabs/puppet/blob/8.5.1/lib/puppet/parser/functions/generate.rb#L34

--- project.clj
+++ project.clj
@@ -131,7 +131,7 @@
                          [puppetlabs/rbac-client ~rbac-client-version]
                          [puppetlabs/rbac-client ~rbac-client-version :classifier "test"]
                          [puppetlabs/analytics-client "1.2.0"]
-                         [puppetlabs/clj-shell-utils "1.0.2"]
+                         [puppetlabs/clj-shell-utils "2.0.1"]
                          [puppetlabs/jruby-utils "5.1.2"]
 
                          ;; When these versions change we need to also
